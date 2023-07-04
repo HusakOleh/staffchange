f21b98d0b8294fafa33210ea26e465ef3caf80a0
HusakOleh <husakoleh91@gmail.com>
2023-07-04 15:07:34 +0300

    fix SSR deeploying

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
